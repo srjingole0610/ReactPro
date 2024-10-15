5a415db53d2147cd4a04e8efce64c42f648bf020
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-15 22:31:52 +0530

    conditionally rendering the JSX using ternary operator

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -3,27 +3,8 @@ import Code from './Code'
 import Welcome from './Welcome'
 
 function ConditionalComponent() {
-
-    // const message1 = <h1>Welcome to Message 1</h1>
-    // const message2 = <h1>Welcome to Message 2</h1>
-    // const display = false;
-
-    // if (display) {
-    //     return message1
-    // }
-    // else {
-    //     return message2
-    // }
-
-    let message;
-    const display = false;
-    if (display) {
-        message = <h1>Welcome to Message 1</h1>;
-    }
-    else {
-        message = <h1>Welcome to message2</h1>
-    }
-    return message
+    const display = false
+    return display ? <h1>Welcome 1</h1> : <h1>Welcome 2</h1>
 }
 
 export default ConditionalComponent
