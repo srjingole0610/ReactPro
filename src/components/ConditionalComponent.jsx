95e76251aefa61c88ddc04fc87c753e782d3e9bb
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-15 22:19:30 +0530

    Rendering the elements with elements Variable

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -3,21 +3,27 @@ import Code from './Code'
 import Welcome from './Welcome'
 
 function ConditionalComponent() {
-    const display = false
+
+    // const message1 = <h1>Welcome to Message 1</h1>
+    // const message2 = <h1>Welcome to Message 2</h1>
+    // const display = false;
+
+    // if (display) {
+    //     return message1
+    // }
+    // else {
+    //     return message2
+    // }
+
+    let message;
+    const display = false;
     if (display) {
-        return (
-            <div>
-                <Code />
-            </div>
-        )
+        message = <h1>Welcome to Message 1</h1>;
     }
     else {
-        return (
-            <div>
-                <Welcome />
-            </div>
-        )
+        message = <h1>Welcome to message2</h1>
     }
+    return message
 }
 
 export default ConditionalComponent
