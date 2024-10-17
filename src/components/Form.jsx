d2e1fc910c360674830ca8047c9c26aeba5eb8f3
Suraj Ingole <74641861+srjingole0610@users.noreply.github.com>
2024-10-17 09:15:46 +0530

    Added changes related to form submit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,14 +11,22 @@ function Form() {
     const handleLastName = (e) => {
         setName({ ...name, lastName: e.target.value })
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name)
+    }
     return (
         <div>
+            <h1>{name.firstName} - {name.middleName} - {name.lastName}</h1>
             <br />
             <form >
                 <input onChange={handleFirstName} type="text" value={name.firstName} />
                 <input onChange={handleMiddleName} type="text" value={name.middleName} />
                 <input onChange={handleLastName} type="text" value={name.lastName} />
+                <button onClick={handleSubmit}>Submit</button>
             </form>
+
         </div>
     )
 }
